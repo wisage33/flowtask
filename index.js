26d93a48abe52bfc09d3e781d139efeeeceb34d6
wisage33 <zellwisage@gmail.com>
2025-04-10 19:00:40 +0400

    Again testing connect

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 5000
 
 app.get("/", (req, res) => {
-  res.send("Hello world")
+  res.send("Hello from gitHub")
 })
 
 app.listen(port, () => console.log("Server listen at http://localhost:5000"))
