95d8844512344387c26e1cedf7065b8093ae38b4
lodoSB <81797166+wisage33@users.noreply.github.com>
2025-04-15 09:05:43 +0000

    Send file on / directory

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express"
+
 const app = express()
 const port = 5000
 
+app.use(express.static(__dirname + "/public"))
+
 app.get("/", (req, res) => {
-  res.send("Hello from laptop")
+  res.sendFile("public/index.html", { root: __dirname })
 })
 
 app.listen(port, () => console.log("Server listen at http://localhost:5000"))
