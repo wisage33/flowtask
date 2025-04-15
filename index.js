4fed08f04dae3dbf5ed7b3bd64cc401e729e3eed
wisage33 <zellwisage@gmail.com>
2025-04-15 11:28:26 +0000

    Send index.html file on / url directory

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-import express from "express"
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app = express()
-const port = 5000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.static(__dirname + "/public"))
+const app = express();
+const port = 5000;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile("public/index.html", { root: __dirname })
-})
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.listen(port, () => console.log("Server listen at http://localhost:5000"))
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
