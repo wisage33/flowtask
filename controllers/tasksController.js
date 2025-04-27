5031d41aaa765de1ced31bd7109cf33c35ce77e9
wisage33 <zellwisage@gmail.com>
2025-04-28 00:59:21 +0400

    Redesign card of cloud servers to modern design

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -3,6 +3,7 @@ let servers = [
     {id: 2, name: 'Google Cloud', status: 'Pending'},
     {id: 3, name: 'Yandex Cloud', status: 'Working'},
     {id: 4, name: 'Microsoft', status: 'Pending'},
+    {id: 5, name: 'CLO.ru', status: 'Error'}
 ]
 
 export const getAll = (req, res) => {
