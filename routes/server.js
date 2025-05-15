f94047f2ad763aeae5bef76f46c4d939cecfdf51
wisage33 <zellwisage@gmail.com>
2025-05-15 19:06:22 +0400

    Add telegramAuth for routes

--- routes/server.js
+++ routes/server.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import taskController from "../controllers/tasksController.js"
 import { getUsers } from "../controllers/usersController.js"
+import { TelegramAuth } from "../middleware/telegramAuth.js"
 
 const router = Router()
 
-router.get('/api/tasks', taskController.getAll)
+router.get('/api/tasks', TelegramAuth, taskController.getAll)
 router.post('/api/tasks', taskController.create)
 router.delete('/api/tasks/:id', taskController.delete)
 router.get('/api/users', getUsers)
