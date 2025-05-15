637def3f7956c0e76fa0f1a05cde7c57cf64d2c4
wisage33 <zellwisage@gmail.com>
2025-05-15 12:33:44 +0400

    Chech all initData

--- config/telegram_bot.js
+++ config/telegram_bot.js
@@ -4,22 +4,15 @@ import "dotenv/config"
 const bot = new TelegramBot(process.env.TG_TOKEN, { polling: true})
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-  
+    
     const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-  
-    // send back the matched "whatever" to the chat
+    const resp = match[1];
+
     bot.sendMessage(chatId, resp);
   });
   
-  // Listen for any kind of message. There are different kinds of
-  // messages.
   bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-  
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
+
+    bot.sendMessage(chatId, JSON.stringify(msg));
   });
