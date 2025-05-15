d9cd409f442406c9a4f61723cc26cd924d0080b1
wisage33 <zellwisage@gmail.com>
2025-05-15 19:02:22 +0400

    Add telegram auth with hmac validate

---
+++ middleware/telegramAuth.js
@@ -0,0 +1,42 @@
+import User from 'models/User.js'
+import crypto from 'crypto'
+
+export const TelegramAuth = async (req, res, next) => {
+    const initData = req.body?.initData || req.query?.initData
+    const {hash, ...data} = initData
+
+    const checkString = Object.keys(data)
+        .map(k => `${k}=${data[k]}`)
+        .sort()
+        .join(`\n`)
+
+    const secret = crypto.createHash('sha256')
+        .update(process.env.TG_TOKEN)
+        .digest()
+
+    const hmac = crypto.createHmac('sha256', secret)
+        .update(checkString)
+        .digest('hex')
+
+    if (hmac !== hash) {
+        res.status(400).json({ error: 'Invalid hash'})
+    }
+
+    const user = data.user
+
+    const dbUser = await User.findOne({ telegramId: user.id})
+
+    if(!dbUser) {
+        dbUser = await User.create({
+            telegramId: user.id,
+            firstName: user.first_name,
+            lastName: user.last_name,
+            username: user.username,
+            photoUrl: user.photo_url
+        })
+    }
+
+    req.user = dbUser
+
+    next();
+}
--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
 import mongoose, { mongo } from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    createdAt: { type: Date, default: Date.now}
+    telegramId: {type: Number, required: true, unique: true},
+    firstName: String,
+    lastName: String,
+    username: String,
+    photoUrl: String,
+    role: {type: String, default: 'user'},
+    createdAt: {type: Date, default: Date.now}
 })
 
 export default mongoose.models.User || mongoose.model('User', userSchema)
