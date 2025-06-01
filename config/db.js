bbba48f326db5bda1748454a374987c833d07c3f
wisage33 <zellwisage@gmail.com>
2025-06-01 18:09:42 +0400

    Fixed connection to database

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/flowtask")
+        const conn = await mongoose.connect("mongodb://127.0.0.1:27017/flowtask")
         console.log("Mongoose connected")
         return conn.connection
     } catch (err) {
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,8 +4,8 @@ import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose';
 
 export const TelegramAuth = async (req, res) => {
-    const { initData } = req.body
-    // const initData = "user=%7B%22id%22%3A6075655377%2C%22first_name%22%3A%22pin%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22lodosb%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F5mjQ-5Ozhgt3Jq-eSyNxJphLabAvqtLJQ-KdAoMyLp6OURt1sTbd3c5INGXOiEEN.svg%22%7D&chat_instance=-2688561892202426043&chat_type=private&auth_date=1747760772&signature=Blk1DLDuHmsfwrM_S0UgGiXAKBxWdRk90bnndqOj0QJvPkVmieottXzdA2wnZlXKDZmTuMrijVv18iAElnlmAw&hash=d520b65050e01b0e3879b325dd885e427f3f0d63275dc8d3825af5b7129b8502"
+    // const { initData } = req.body
+    const initData = "user=%7B%22id%22%3A6075655377%2C%22first_name%22%3A%22pin%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22lodosb%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F5mjQ-5Ozhgt3Jq-eSyNxJphLabAvqtLJQ-KdAoMyLp6OURt1sTbd3c5INGXOiEEN.svg%22%7D&chat_instance=-2688561892202426043&chat_type=private&auth_date=1747760772&signature=Blk1DLDuHmsfwrM_S0UgGiXAKBxWdRk90bnndqOj0QJvPkVmieottXzdA2wnZlXKDZmTuMrijVv18iAElnlmAw&hash=d520b65050e01b0e3879b325dd885e427f3f0d63275dc8d3825af5b7129b8502"
     const params = new URLSearchParams(initData)
     const hash = params.get('hash')
     params.delete('hash')
